48c4f8ebf8beb991afc79e2df0a43f94482c0a55
mprentice97 <52010473+mprentice97@users.noreply.github.com>
2019-06-24 16:58:56 -0400

    Add files via upload

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,17 +10,25 @@ namespace HeroTesting
         [TestMethod]
         public void should_add_hero()
         {
-            var result = _HeroList.addHero(int);
+            var hero = new Hero();
+            hero.name = "Dale";
 
-            result.Make.ShouldEqual(5);
+            heros.Add(new Hero { Id = 0, Name = "Chris", Pic = "https://making-the-web.com/sites/default/files/clipart/142014/stick-figure-142014-5551841.jpg", Power = 1.5 });
+            heros.Add(new Hero { Id = 1, Name = "Marcus", Pic = "https://image.shutterstock.com/image-vector/stick-figure-celebration-cheer-260nw-331595411.jpg", Power = 2 });
+            heros.Add(new Hero { Id = 2, Name = "Ashazi", Pic = "http://clipart-library.com/images/pio5eXK6T.png", Power = 3 });
+            heros.Add(new Hero { Id = 3, Name = "Pasha", Pic = "https://image.shutterstock.com/image-vector/stick-figure-business-ideas-260nw-220840597.jpg", Power = 4 });
+            heros.Add(new Hero { Id = 4, Name = "Dale", Pic = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2014%2F11%2F11%2F01%2F24%2Fstrong-526265_960_720.jpg&imgrefurl=https%3A%2F%2Fpixabay.com%2Fillustrations%2Fstrong-stick-figure-adult-happy-526265%2F&docid=ExWGomSs9u8G9M&tbnid=8OGGvW0IblR0AM%3A&vet=10ahUKEwjlz_GE9YLjAhWxc98KHYmvDtoQMwhaKAEwAQ..i&w=478&h=720&safe=active&bih=969&biw=1920&q=strong%20stick%20figure&ved=0ahUKEwjlz_GE9YLjAhWxc98KHYmvDtoQMwhaKAEwAQ&iact=mrc&uact=8 ", Power = 2.5 });
 
-            //add Heroes[Test]  
+            heros.addHero(hero);
         }
+            //add Heroes[Test]  
+    }
 
         [TestMethod]
-        public void should_create_get_hero()
+        public void should_get_hero()
         {
-            var result = _HeroList.getHero("S");
+            var hero = Hero(int id);
+            heros.getHero = 2;
 
             result.Make.ShouldEqual("Chris");
             //get Heroes[Test]
@@ -29,7 +37,8 @@ namespace HeroTesting
         [TestMethod]
         public void should_update_hero()
         {
-            var result = _HeroList.updateHero(int);
+            
+
 
             result.Make.ShouldEqual(5);
 
@@ -38,12 +47,18 @@ namespace HeroTesting
 
         [TestMethod]
         public void should_remove_hero()
-        {
-            var result = _HeroList.removeHero(int);
+        { 
+             var hero = Hero(int id);
+             heros.removeHero = 4;
 
-            result.Make.ShouldEqual(4);
+                heros.Add(new Hero { Id = 0, Name = "Chris", Pic = "https://making-the-web.com/sites/default/files/clipart/142014/stick-figure-142014-5551841.jpg", Power = 1.5 });
+                heros.Add(new Hero { Id = 1, Name = "Marcus", Pic = "https://image.shutterstock.com/image-vector/stick-figure-celebration-cheer-260nw-331595411.jpg", Power = 2 });
+                heros.Add(new Hero { Id = 2, Name = "Ashazi", Pic = "http://clipart-library.com/images/pio5eXK6T.png", Power = 3 });
+                heros.Add(new Hero { Id = 3, Name = "Pasha", Pic = "https://image.shutterstock.com/image-vector/stick-figure-business-ideas-260nw-220840597.jpg", Power = 4 });
+                heros.Add(new Hero { Id = 4, Name = "Dale", Pic = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2014%2F11%2F11%2F01%2F24%2Fstrong-526265_960_720.jpg&imgrefurl=https%3A%2F%2Fpixabay.com%2Fillustrations%2Fstrong-stick-figure-adult-happy-526265%2F&docid=ExWGomSs9u8G9M&tbnid=8OGGvW0IblR0AM%3A&vet=10ahUKEwjlz_GE9YLjAhWxc98KHYmvDtoQMwhaKAEwAQ..i&w=478&h=720&safe=active&bih=969&biw=1920&q=strong%20stick%20figure&ved=0ahUKEwjlz_GE9YLjAhWxc98KHYmvDtoQMwhaKAEwAQ&iact=mrc&uact=8 ", Power = 2.5 });
 
+
+            HeroList.Remove(Id = 4);
             //subtract Heroes[Test]
         }
-    }
 }
